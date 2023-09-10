112bf81db0c98957240a32eb2188d3319af979c8
AlexiSkyline <ilegal_sprite@outlook.com>
2023-09-10 00:14:10 -0600

    dev: initial project

---
+++ src/test/java/com/skyline/forum/ForumAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyline.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
